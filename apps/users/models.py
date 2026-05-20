bb94f0783e10bd96684933289723ecd3be802808
Tobydou21 <tr76846@gmail.com>
2026-05-20 14:06:02 -0300

    feat: modelo User personalizado con bio, avatar y following

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
