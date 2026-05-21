558ac1ef2dd3d8c6af3a385442057f8d93d1a986
Tobydou21 <tr76846@gmail.com>
2026-05-20 21:25:14 -0300

    feat: endpoint feed personalizado con JWT

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import PostViewSet, feed_view
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+router.register(r'', PostViewSet, basename='post')
 
 urlpatterns = [
+    path('feed/', feed_view, name='feed'),
     path('', include(router.urls)),
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 from .models import Post
 from .serializers import PostSerializer
 
@@ -12,3 +13,14 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def feed_view(request):
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(
+        user__in=following_users
+    ).select_related('user').order_by('-created_at')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
