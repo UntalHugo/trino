fef4b865f52f3fd563aae038995d8db1b92a34a7
hugosaspy <hugo.ayala.668@alumnos.uninorte.edu.py>
2026-05-30 10:31:24 -0300

    feat: agregar modelo Hashtag y extraccion automatica con regex

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.conf import settings
+import re
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'#{self.name}'
+
 
 class Post(models.Model):
     user = models.ForeignKey(
@@ -9,11 +19,20 @@ class Post(models.Model):
     )
     content = models.TextField(max_length=280)
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    hashtags = models.ManyToManyField(Hashtag, blank=True, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.hashtags.clear()
+        tags = re.findall(r'#(\w+)', self.content)
+        for tag in tags:
+            hashtag, _ = Hashtag.objects.get_or_create(name=tag.lower())
+            self.hashtags.add(hashtag)
 
     def __str__(self):
         return f"{self.user.username} - {self.content[:50]}"
--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Hashtag
+
+
+class HashtagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hashtag
+        fields = ['id', 'name']
+
 
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
+    hashtags = HashtagSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'user', 'content', 'image', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'user', 'created_at', 'updated_at']
+        fields = ['id', 'user', 'content', 'image', 'hashtags', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'user', 'created_at', 'updated_at', 'hashtags']
